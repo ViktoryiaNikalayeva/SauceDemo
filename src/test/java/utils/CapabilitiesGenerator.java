092bde05bf5285013a5e3d10e3ad6dfc95aa9188
Viktoryia Nikalayeva <vik.nik.09.11.1995@gmail.com>
2020-10-30 20:33:28 +0300

    linux

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -12,7 +12,7 @@ public class CapabilitiesGenerator {
         if (os.contains("win")) {
             System.setProperty("webdriver.chrome.driver", driverPath + "/chromedriver.exe");
         } else if (os.contains("mac")) {
-            System.setProperty("webdriver.chrome.driver", driverPath + "/webdrivers/Linux/chromedriver");
+            System.setProperty("webdriver.chrome.driver", driverPath + "/webdrivers/linux/chromedriver");
         } else {
             System.setProperty("webdriver.chrome.driver", driverPath + "/linux/chromedriver");
         }
